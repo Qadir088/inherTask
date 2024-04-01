0996fb6ce27fb8026a6934a9f0343232684cc5cc
Qadir088 <qadircabbarli55@gmail.com>
2024-04-01 07:51:21 +0400

    task solved

---
+++ ConsoleApp15/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age {  get; set; }
+        public int[] Grades { get; set; } = new int[0];
+
+        public Human(string name, string surname,byte age, int[] grades) 
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            for(int i = 0; i < Grades.Length; i++)
+            {
+                if (grades[i] %2== 0)
+                {
+                    Grades = Grades.Append(grades[i]).ToArray();
+                }
+            }
+        
+        }
+        public void getNum(Human  human)
+        {
+            foreach (var item in human.Grades)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human("Ali","Aliev",26,new int[] {1,2,3,4,5,6,7,8,9,10});
+            human.getNum(human);
+        }
+    }
+}
